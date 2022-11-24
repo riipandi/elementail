f78f7a991b6035e6006415fc0602a16078eac75a
Aris Ripandi <riipandi@gmail.com>
2022-11-25 03:26:57 +0700

    add husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `@elementail/eslint-config`
-  extends: ["elementail"],
+  extends: ["@elementail/eslint-config"],
   settings: {
     next: {
       rootDir: ["apps/*/"],
