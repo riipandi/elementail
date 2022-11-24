dff473a74371175bc283bc05190ecaef8beaf10b
Aris Ripandi <riipandi@gmail.com>
2022-11-25 04:07:11 +0700

    add examples react and nextjs

--- examples/nextjs/src/pages/_app.tsx
+++ examples/nextjs/src/pages/_app.tsx
@@ -3,5 +3,5 @@ import '@elementail/core/tailwind.css'
 // import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+  return <Component {...pageProps} />
 }
--- examples/vite-react/src/main.tsx
+++ examples/vite-react/src/main.tsx
@@ -5,7 +5,7 @@ import '@elementail/core/styles.css'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
