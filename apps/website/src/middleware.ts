2b8cc7e4d7fc7c6d817b2eb39801d103e762c2f7
Aris Ripandi <riipandi@gmail.com>
2023-01-28 20:20:59 +0700

    redirect website to twistail.com

---
+++ apps/website/src/middleware.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const url = request.nextUrl.clone()
+  if (url.pathname === '/') {
+    return NextResponse.redirect('https://twistail.com')
+  }
+}
