36d95e17750d1f007cffff6f2caf4a958a114e11
riipandi <riipandi@gmail.com>
2023-01-28 13:27:23 +0000

    Deploying to gh-pages from @ riipandi/elementail@14eeae8086eef38ab96c592285f5eb56585279a2 🚀

---
+++ _next/static/Y8hfgqoGq7rK64xjUpKE-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-625f5a54ca58f19f.js"],"/_error":["static/chunks/pages/_error-15a0bbaa3c2350f1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
